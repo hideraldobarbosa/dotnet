2b9e8ebb8af25cf2e85fd50443892a3ffe753063
hideraldobarbosa <hideraldo.barbosa@gmail.com>
2020-05-06 22:27:38 -0300

    tratamento de configurações - User Secrets - Dominando AS -

--- AspNetCoreIdentity/AspNetCoreIdentity/Config/IdentityConfig.cs
+++ AspNetCoreIdentity/AspNetCoreIdentity/Config/IdentityConfig.cs
@@ -1,9 +1,10 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using AspNetCoreIdentity.Extensions;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
+using AspNetCoreIdentity.Areas.Identity.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
 
 namespace AspNetCoreIdentity.Config
 {
@@ -25,6 +26,17 @@ namespace AspNetCoreIdentity.Config
             });
             return services;
         }
+        public static IServiceCollection AddIdentityConfig(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<AspNetCoreIdentityContext>(options =>
+                options.UseSqlServer(
+                   configuration.GetConnectionString("AspNetCoreIdentityContextConnection")));
 
+            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddRoles<IdentityRole>()
+                .AddEntityFrameworkStores<AspNetCoreIdentityContext>();
+
+            return services;
+        }
     }
 }
